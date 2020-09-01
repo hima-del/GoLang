f1a66008fdbd90aceba3b5587febdc5b1eaba09d
hima-del <himajaharidas77@gmail.com>
2020-09-01 12:06:41 +0530

    exercises

---
+++ 085_exercise_1.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+	y := "James Bond"
+	z := true
+	fmt.Printf("x:%d\ny:%s\nz:%t\n", x, y, z)
+	fmt.Printf("x:%d\n", x)
+	fmt.Printf("y:%s\n", y)
+	fmt.Printf("z:%t\n", z)
+}
