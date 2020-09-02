eec790d6e790e15fc57cd105d8e7b1a2ca0df589
hima-del <himajaharidas77@gmail.com>
2020-09-02 19:39:53 +0530

    maps

---
+++ 117_exercise_28.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	detail := map[string][]string{
+		"bond_james":      []string{"shaken, not stirred", "martinis", "women"},
+		"moneypenny_miss": []string{"james bond", "literature", "computer science"},
+		"no_dr":           []string{"being evil", "ice cream", "sunsets"},
+	}
+	fmt.Println(detail)
+	detail["james_annie"] = []string{"agar agar", "chocolate", "football"}
+	for key, value1 := range detail {
+		fmt.Println("this is the record of ", key)
+		for i, value2 := range value1 {
+			fmt.Println(i, value2)
+		}
+	}
+}
