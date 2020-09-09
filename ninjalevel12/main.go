5f9087bc2fde582c4861addcec771fda9680deb4
hima-del <himajaharidas77@gmail.com>
2020-09-09 08:29:51 +0530

    documentation

---
+++ ninjalevel12/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+type canine struct {
+	name string
+	age  int
+}
+
+func main() {
+	fido := canine{
+		name: "Fido",
+		age:  dog.Years(10),
+	}
+	fmt.Println(fido)
+}
