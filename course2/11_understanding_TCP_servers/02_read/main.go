5c0c8f06090296290c97215d5ffcebeb76bad898
hima-del <himajaharidas77@gmail.com>
2020-09-17 16:08:02 +0530

    read and write

---
+++ course2/11_understanding_TCP_servers/02_read/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080") //we get listener
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer li.Close()
+
+	for { //looping over the listener to get a cconnection
+		conn, err := li.Accept()
+		if err != nil {
+			log.Fatalln(err)
+			continue
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+	}
+	defer conn.Close()
+	fmt.Println("code got here")
+}
