544763fea63f7d5653ddde77621bfb6d71c18165
hima-del <himajaharidas77@gmail.com>
2020-09-27 23:17:58 +0530

    sessions

---
+++ course2/24_sessions/01_uuid/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func foo(w http.ResponseWriter, req *http.Request) {
+	cookie, err := req.Cookie("session")
+	if err != nil {
+		id, err := uuid.NewV4()
+		if err != nil {
+			fmt.Println(err)
+		}
+		cookie = &http.Cookie{
+			Name:  "session",
+			Value: id.String(),
+			//secure:true,
+			HttpOnly: true,
+			Path:     "/",
+		}
+		http.SetCookie(w, cookie)
+	}
+	fmt.Println(cookie)
+}
