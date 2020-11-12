d2f52dc5cce24e75fdf32e7495961d20c7ea1101
hima-del <himajaharidas77@gmail.com>
2020-11-12 23:54:18 +0530

    learning gingonic package

---
+++ 09_My_Codes/01/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	//Default returns an Engine instance with the Logger and Recovery middleware already attached.
+	router := gin.Default()
+
+}
