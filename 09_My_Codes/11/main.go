b8d652adce04d5cfd4709fa77ba541acbe7a5194
hima-del <himajaharidas77@gmail.com>
2020-11-19 13:42:03 +0530

    channels

---
+++ 09_My_Codes/11/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	ch := make(chan int)
+	go producer(ch)
+	for {
+		v, ok := <-ch
+		if ok == false {
+			break
+		}
+		fmt.Println("recieved", v, ok)
+	}
+}
+
+func producer(ch chan int) {
+	for i := 0; i < 10; i++ {
+		ch <- i
+	}
+	close(ch)
+}
